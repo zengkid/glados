4ca6934625952634026f8b48d3c115d63df52c8e
ZengKid <zengkid@msn.com>
2021-10-16 20:02:49 +0800

    update

--- bin/glados.dart
+++ bin/glados.dart
@@ -9,7 +9,7 @@ Future<void> main(List<String> arguments) async {
   // var useragent =
   //     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36";
 
-  var cookie = Platform.environment['cookie'] ??
+  var cookie = Platform.environment['COOKIE'] ??
       '_ga=GA1.2.1419742385.1630674104; koa:sess=eyJ1c2VySWQiOjY2Nzk1LCJfZXhwaXJlIjoxNjU5NDA5NjQyOTkzLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=kN66-kf_ia0x_UxI1j_cALtHOdQ; _gid=GA1.2.1814523799.1634375787; _gat_gtag_UA_104464600_2=1';
   print('cookie = $cookie');
 
