895f39d8f4e8f1222a3e89facebc11a5f9061284
zengkid <zengkid@msn.com>
2022-03-05 19:32:01 +0800

    update header

--- bin/glados.dart
+++ bin/glados.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 
 Future<void> main(List<String> arguments) async {
-  var url = "https://glados.rocks/api/user/checkin";
+  var url = 'https://glados.rocks/api/user/checkin';
   // var origin = "https://glados.rocks";
   // var referer = "https://glados.rocks/console/checkin";
   // var useragent =
@@ -14,10 +14,22 @@ Future<void> main(List<String> arguments) async {
   var payload = json.encode({'token': 'glados_network'});
   var response = await http.post(Uri.parse(url),
       headers: {
+        // ":authority": 'glados.rocks',
+        // ":method": 'POST',
+        // ":path": '/api/user/checkin',
+        // ":scheme": 'https',
         'content-type': 'application/json;charset=UTF-8',
         'cookie': cookie,
+        'dnt': '1',
+        'origin': 'https://glados.rocks',
+        'sec-ch-ua':
+            '" Not;A Brand";v="99", "Microsoft Edge";v="97", "Chromium";v="97"',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 Edg/97.0.1072.76',
       },
       body: payload);
-  // http.post(url, )
   print(response.body);
 }
